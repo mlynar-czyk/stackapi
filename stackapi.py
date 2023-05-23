0c298d17570efd2c9d034bd4e282fc72e9e8529f
mlynar-czyk <64000170+mlynar-czyk@users.noreply.github.com>
2023-05-23 21:08:32 +0000

    On branch filter_improvment Changes to be committed: 	modified:   stackapi.py
    Improve filter

--- stackapi.py
+++ stackapi.py
@@ -11,39 +11,45 @@ api_key = os.getenv("STACKEXCHANGE_API_KEY")
 env_site = os.getenv("STACKEXCHANGE_SITE")
 env_tag = os.getenv("TAG")
 env_date = os.getenv("FROM_DATE")
+filter = "!BLgpu)DfJ(d9Rhoza_YLz8n3TIrIvY"
 
 
 # Convert the date in UNIX timestamp format
 
 date_obj = datetime.datetime.strptime(env_date, "%Y-%m-%d")
-timestamp = time.mktime(date_obj.timetuple())
+timestamp = int(time.mktime(date_obj.timetuple()))
 
 def fetch_questions(tag, date, site):
     base_url = "https://api.stackexchange.com/2.3/questions"
     params = {
-        "order": "asc",
-        "sort": "creation",
+        "order": "desc",
+        "sort": "votes",
         "tagged": tag,
         "site": site,
-        "filter": "!9Z(-wzu0T",
-        "fromdate": date
+        "filter": filter,
+        "fromdate": date,
+        "is_answered": True
     }
 
     response = requests.get(base_url, params=params)
     data = response.json()
+    print(response.url)
     return data["items"]
+
 
 def fetch_answers(question_id, site):
     base_url = f"https://api.stackexchange.com/2.3/questions/{question_id}/answers"
     params = {
-        "order": "asc",
-        "sort": "creation",
+        "order": "desc",
+        "sort": "votes",
         "site": site,
-        "filter": "!9Z(-wzu0T"
+        "filter": filter,
+        "is_accepted": True
     }
 
     response = requests.get(base_url, params=params)
     data = response.json()
+    print(response.url)
     return data["items"]
 
 def save_to_json(file_path, questions_with_answers):
@@ -53,8 +59,8 @@ def save_to_json(file_path, questions_with_answers):
 def main():
     folder_path = "."  # JSON folder path
     tag = env_tag # stackexchange tag
-    date = int(timestamp) # UNIX timestamp date
-    site = str(env_site)  # Stackexchange site (for example : "stackoverflow")
+    date = timestamp # UNIX timestamp date
+    site = env_site  # Stackexchange site (for example : "stackoverflow")
 
     questions = fetch_questions(tag, date, site)
     questions_with_answers = []
@@ -62,11 +68,12 @@ def main():
     for question in questions:
         question_id = question["question_id"]
         answers = fetch_answers(question_id, site)
+        
         question_data = {
             "question": question,
             "answers": answers
         }
-        questions_with_answers.append(question_data)
+        questions_with_answers.append(question_data)       
 
     file_path = f"{folder_path}/questions_with_answers.json"
     save_to_json(file_path, questions_with_answers)
