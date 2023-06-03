a89c12d4194b347837fbcfe834497450c94396ad
mlynar-czyk <64000170+mlynar-czyk@users.noreply.github.com>
2023-06-03 07:15:56 +0000

    Code cleaning

--- stackapi.py
+++ stackapi.py
@@ -1,8 +1,9 @@
-import requests
+'''Module providing JSON handling capability '''
 import json
 import os
 import datetime
 import time
+import requests
 from dotenv import load_dotenv
 
 
@@ -11,7 +12,7 @@ api_key = os.getenv("STACKEXCHANGE_API_KEY")
 env_site = os.getenv("STACKEXCHANGE_SITE")
 env_tag = os.getenv("TAG")
 env_date = os.getenv("FROM_DATE")
-filter = "!BLgpu)DfJ(d9Rhoza_YLz8n3TIrIvY"
+FILTER = "!BLgpu)DfJ(d9Rhoza_YLz8n3TIrIvY"
 
 
 # Convert the date in UNIX timestamp format
@@ -21,45 +22,49 @@ timestamp = int(time.mktime(date_obj.timetuple()))
 
 
 def fetch_questions(tag, date, site):
+    '''Fetch the questions from a given stackexchange site with a given tag'''
     base_url = "https://api.stackexchange.com/2.3/questions"
     params = {
         "order": "desc",
         "sort": "votes",
         "tagged": tag,
         "site": site,
-        "filter": filter,
+        "filter": FILTER,
         "fromdate": date,
         "is_answered": True,
     }
 
-    response = requests.get(base_url, params=params)
+    response = requests.get(base_url, params=params, timeout=10)
     data = response.json()
     print(response.url)
     return data["items"]
 
 
 def fetch_answers(question_id, site):
+    '''Fetch answers from given question_id from a given stackexchange site'''
     base_url = f"https://api.stackexchange.com/2.3/questions/{question_id}/answers"
     params = {
         "order": "desc",
         "sort": "votes",
         "site": site,
-        "filter": filter,
+        "filter": FILTER,
         "is_accepted": True,
     }
 
-    response = requests.get(base_url, params=params)
+    response = requests.get(base_url, params=params, timeout=10)
     data = response.json()
     print(response.url)
     return data["items"]
 
 
 def save_to_json(file_path, questions_with_answers):
-    with open(file_path, "w") as file:
+    '''Save the questions with corresponding answers in a JSON file'''
+    with open(file_path, "w", encoding='utf-8') as file:
         json.dump(questions_with_answers, file, indent=4)
 
 
 def main():
+    '''Run the stackapi'''
     folder_path = "."  # JSON folder path
     tag = env_tag  # stackexchange tag
     date = timestamp  # UNIX timestamp date
